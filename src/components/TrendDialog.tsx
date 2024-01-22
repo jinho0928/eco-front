daadfcf08dc42e25f24bcb344213eb491b02df40
deuckyoung_cho <deuckyoung_cho@tmax.co.kr>
2024-01-22 17:51:14 +0900

    fix: average 추가

--- src/components/TrendDialog.tsx
+++ src/components/TrendDialog.tsx
@@ -10,114 +10,115 @@ import { useEffect } from "react";
 const serverUrl = import.meta.env.VITE_SERVER_URL;
 
 export function TrendDialog({ open, onClose, onSuccess, files }) {
-    const store = useLocalObservable(() => ({
-        date: "",
-        items: [],
-        keys: [],
-        isOpen: false,
-        isLoaded: false,
+  const store = useLocalObservable(() => ({
+    date: "",
+    items: [],
+    keys: [],
+    isOpen: false,
+    isLoaded: false,
 
-        async addTrend() {
-            const date = DateTime.fromFormat(`${store.date}`, "yyMMdd").toFormat(
-                "yyyy-MM-dd"
-            );
-            const list = toJS(store.items).map(({ skuid, current, ...dates }) => {
-                return {
-                    skuid,
-                    inventory: current,
-                    dates: Object.entries(dates).reduce((acc, [key, value]) => {
-                        acc.push({ date: key, value })
-                        return acc;
-                    }, [])
-                }
-            });
-
-
-            try {
-                await axios.post(`${serverUrl}/trend`, {
-                    list,
-                });
-            } catch (err) {
-                console.error(err);
-            }
-        },
-
-        get columns() {
-            return [
-                {
-                    key: "skuid",
-                    name: "SKU ID",
-                },
-                {
-                    key: "current",
-                    name: "현재고",
-                },
-                {
-                    key: "average",
-                    name: "평균판매량",
-                },
-                ...this.keys.slice(3).map((key) => ({ key, name: key }))
-            ]
+    async addTrend() {
+      const date = DateTime.fromFormat(`${store.date}`, "yyMMdd").toFormat(
+        "yyyy-MM-dd"
+      );
+      const list = toJS(store.items).map(
+        ({ skuid, current, average, ...dates }) => {
+          return {
+            skuid,
+            inventory: current,
+            dates: Object.entries(dates).reduce((acc, [key, value]) => {
+              acc.push({ date: key, value });
+              return acc;
+            }, []),
+          };
         }
-    }));
+      );
 
-    useEffect(() => {
-        (async () => {
-            const buffer = await readFileAsArrayBuffer(files[0]);
-            const { rows, keys } = await parseTrend({ arrayBuffer: buffer });
+      try {
+        await axios.post(`${serverUrl}/trend`, {
+          list,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    },
 
-            transaction(() => {
-                store.items = rows;
-                store.keys = keys;
-                store.isLoaded = true;
-            });
-        })();
-    }, []);
+    get columns() {
+      return [
+        {
+          key: "skuid",
+          name: "SKU ID",
+        },
+        {
+          key: "current",
+          name: "현재고",
+        },
+        {
+          key: "average",
+          name: "평균판매량",
+        },
+        ...this.keys.slice(3).map((key) => ({ key, name: key })),
+      ];
+    },
+  }));
 
-    const handleAdd = async () => {
-        await store.addTrend();
-        onSuccess();
-    };
+  useEffect(() => {
+    (async () => {
+      const buffer = await readFileAsArrayBuffer(files[0]);
+      const { rows, keys } = await parseTrend({ arrayBuffer: buffer });
 
-    return (
-        <Dialog open={open} onClose={onClose}>
-            <strong style={{ fontSize: 20 }}>{`판매추이`}</strong>
-            <Observer>
-                {() =>
-                    store.isLoaded && (
-                        <>
-                            <Observer>
-                                {() => (
-                                    <DataGrid
-                                        style={{ width: "1000px" }}
-                                        rows={store.items}
-                                        columns={store.columns}
-                                    />
-                                )}
-                            </Observer>
-                        </>
-                    )
-                }
-            </Observer>
-            <div style={{ display: "flex", gap: "15px" }}>
-                <Observer>
-                    {() => (
-                        <Button
-                            fullWidth
-                            variant="contained"
-                            onClick={handleAdd}
-                            disabled={!store.isLoaded}
-                        >
-                            저장
-                        </Button>
-                    )}
-                </Observer>
-                <Button fullWidth variant="outlined" onClick={onClose}>
-                    취소
-                </Button>
-            </div>
-        </Dialog>
-    );
+      transaction(() => {
+        store.items = rows;
+        store.keys = keys;
+        store.isLoaded = true;
+      });
+    })();
+  }, []);
+
+  const handleAdd = async () => {
+    await store.addTrend();
+    onSuccess();
+  };
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <strong style={{ fontSize: 20 }}>{`판매추이`}</strong>
+      <Observer>
+        {() =>
+          store.isLoaded && (
+            <>
+              <Observer>
+                {() => (
+                  <DataGrid
+                    style={{ width: "1000px" }}
+                    rows={store.items}
+                    columns={store.columns}
+                  />
+                )}
+              </Observer>
+            </>
+          )
+        }
+      </Observer>
+      <div style={{ display: "flex", gap: "15px" }}>
+        <Observer>
+          {() => (
+            <Button
+              fullWidth
+              variant="contained"
+              onClick={handleAdd}
+              disabled={!store.isLoaded}
+            >
+              저장
+            </Button>
+          )}
+        </Observer>
+        <Button fullWidth variant="outlined" onClick={onClose}>
+          취소
+        </Button>
+      </div>
+    </Dialog>
+  );
 }
 
 const Dialog = styled(MuiDialog)`
@@ -131,5 +132,3 @@ const Dialog = styled(MuiDialog)`
     max-width: none;
   }
 `;
-
-
